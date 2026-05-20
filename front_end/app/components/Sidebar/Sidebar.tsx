6018c87f1513a81271b0f7541afd303cb0d07d22
RyutaC2 <67426081+RyutaC2@users.noreply.github.com>
2026-05-20 14:34:47 +0900

    fix: buttonを用いたspaからlinkを使ったspaに変更
    - buttonのclickをトリガーとして画面遷移させるのではなく、linkをそのまま使えるそうなので変更

--- front_end/app/components/Sidebar/Sidebar.tsx
+++ front_end/app/components/Sidebar/Sidebar.tsx
@@ -1,24 +1,21 @@
 "use client";
 
 import styles from "./sidebar.module.css";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface SidebarProps {
     isOpen: boolean;
 }
 
 export default function Sidebar({isOpen}: SidebarProps) {
-
-    const router = useRouter();
-
     return (
         <nav className={`${styles.sidebar} ${isOpen ? styles.open : ""} bg-[#242424] dark:bg-[#656565]`}>
             <ul className={`${styles["sidebar-list"]} mt-16 sm:mt-24`}>
-                <li><button onClick={() => router.push("/")}>cd ~</button></li>
-                <li><button onClick={() => router.push("/about")}>whoami</button></li>
-                <li><button onClick={() => router.push("/about/hobby")}>echo $HOBBY</button></li>
-                <li><button onClick={() => router.push("/about/tech")}>cat .my-tech</button></li>
-                <li><button onClick={() => router.push("/about/contact")}>ssh ryuta@contact</button></li>
+                <li><Link href="/">cd ~</Link></li>
+                <li><Link href="/about">whoami</Link></li>
+                <li><Link href="/about/hobby">echo $HOBBY</Link></li>
+                <li><Link href="/about/tech">cat .my-tech</Link></li>
+                <li><Link href="/about/contact">ssh ryuta@contact</Link></li>
             </ul>
         </nav>
     );
